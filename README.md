5809b4096eaabb31cad623c20a790aed8cafef54
JB <jbb@jbb.dev>
2022-11-30 14:55:36 -0800

    aoc initial commit

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# aoc2022
+advent of code 2022
+===================
 
-## anthony's leaderboard: 371564-36ad28cd
+https://adventofcode.com/2022
+
+Thanks to anthonywritescode for this super easy way to grab input.txt and submit the answers to advent of code!
+
+[Visit Anthony's github](https://github.com/anthonywritescode)
+
+### timing
+
+- comparing to these numbers isn't necessarily useful
+- normalize your timing to day 1 part 1 and compare
+- alternate implementations are listed in parens
+- these timings are very non-scientific (sample size 1)
+
+```console
+$ find -maxdepth 1 -type d -name 'day*' -not -name day00 | sort | xargs --replace bash -xc 'python {}/part1.py {}/input.txt; python {}/part2.py {}/input.txt'
++ python ./day01/part1.py ./day01/input.txt
+74
+> 1272 μs
+```
---
+++ day01/part1.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    n = 0
+    l = s.strip().split('\n')
+    for i, c in enumerate(l[1:]):
+        if int(c) > int(l[i]):
+            n += 1
+        # if c == '(':
+        #     n += 1
+        # elif c == ')':
+        #     n -= 1
+        # else:
+        #     raise AssertionError(f'unexpected: {c!r}')
+    return n
+
+
+INPUT_S = '''\
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+'''
+EXPECTED = 7
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
