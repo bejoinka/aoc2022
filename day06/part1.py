72e541f5bb5188bfa2b5364b1cc717a031cd178f
JB <jbb@jbb.dev>
2022-12-05 21:16:16 -0800

    day 6

---
+++ day06/part1.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+def compute(s: str) -> int:
+    n = 0
+    l = s.strip().split('\n')
+    for _, c in enumerate(l):
+        for i, char in enumerate(c):
+            if i < 13:
+                continue
+            s = set([f for f in c[i-14:i]])
+            if len(s) == 14:
+                return i
+    return 0
+
+
+INPUT_S = '''mjqjpqmgbljsphdztnvjfqwrcgsmlb
+'''
+EXPECTED = 7
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
